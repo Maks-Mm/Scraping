1fa1d661c3bf0a32b910749fb8a89585f0cd3b41
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-12-12 12:04:25 +0100

    getLatestArticleList new code blocks and dates Articele

--- utils/article-scraper.js
+++ utils/article-scraper.js
@@ -1,8 +1,13 @@
-import ArticleScraper from "../lib/ArticleScraper.js"
+import ArticleScraper from "../lib/ArticleScraper.js";
+import { getLatestArticleList, saveArticle } from "../lib/db.js";
 
-(async () => {
-  const url =
-    "https://www.blg-logistics.com/presse/suedhafen-schwer-im-geschaeft-umschlag-und-montage-von-kraftwerksmodulen-auf-neuer-blg-flaeche-in-bremerhaven"; // URL статьи
+const pause = async (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const url =
+  "https://www.blg-logistics.com/presse/suedhafen-schwer-im-geschaeft-umschlag-und-montage-von-kraftwerksmodulen-auf-neuer-blg-flaeche-in-bremerhaven"; // URL статьи
+const articleScrap = async (url) => {
   const selectors = {
     titleSelector: "header h1",
     contentSelector: ".ce-bodytext",
@@ -13,8 +18,17 @@ import ArticleScraper from "../lib/ArticleScraper.js"
   const articleData = await scraper.execute();
 
   if (articleData) {
+    await saveArticle(articleData);
     console.log("Данные статьи:", articleData);
   } else {
     console.log("Не удалось извлечь данные статьи.");
   }
-})();
+};
+const obj = await getLatestArticleList("BLG Logistik");
+//console.log(obj);
+obj.articleList.map(async (article, index) => {
+  await pause(index * 1137);
+  console.log(article.link);
+  await articleScrap(article.link);
+});
+//await articleScrap(url);
