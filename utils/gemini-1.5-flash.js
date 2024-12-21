37e41b9c25aad174c5caafb172384f86bd3bd870
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-12-21 11:05:37 +0100

    added  new Library npm i @google/generative-ai

--- utils/gemini-1.5-flash.js
+++ utils/gemini-1.5-flash.js
@@ -1,43 +1,39 @@
-import axios from "axios";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { getArticles } from "../lib/db.js";
 
+// Константы
 const API_KEY = "AIzaSyC32GN759QiVJZlEQLa5R2fML53h0q9zEQ";
+const siteName = "BLG Logistik";
 
-const data = {
-  contents: [
-    {
-      parts: [
-        {
-          text: "Explain how AI works,answer in russian language",
-        },
-      ],
-    },
-  ],
-};
+// Создание экземпляра GoogleGenerativeAI
+const genAI = new GoogleGenerativeAI(API_KEY);
 
-const config = {
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
-
+// Функция для анализа статьи и генерации контента
 async function generateContent() {
   try {
-    const response = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
-      data,
-      config
-    );
-    console.log("Response:", response.data);
-    response.data.candidates.forEach((element) => {
-      console.log(element.content);
-    });
+    // Получение статей из базы данных
+    const articles = await getArticles(siteName);
+    const article = articles[0];
+
+    if (!article) {
+      console.error("Статья не найдена!");
+      return;
+    }
+
+    // Инициализация модели
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    // Отправка запроса к API
+    const response = await model.generateContent([
+      `answer in russian language. You must analyze the article: ${article}`,
+    ]);
+
+    // Обработка ответа API
+    console.log("Ответ:", response.response.text());
   } catch (error) {
-    console.error(
-      "Error:",
-      error.response ? error.response.data : error.message
-    );
+    console.error("Ошибка:", error.message);
   }
 }
 
-// Вызов функции
+// Запуск основной логики
 generateContent();
