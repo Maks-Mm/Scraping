9ee43f52604a34215bad106776faa5c23c638587
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-12-24 11:10:50 +0100

    removing duplicates

--- utils/remove-article-duplicates.js
+++ utils/remove-article-duplicates.js
@@ -1,7 +1,18 @@
-import { getArticles } from "../lib/db.js";
+import { getArticles, deleteArticle } from "../lib/db.js";
 
 const articles = await getArticles();
-console.log(articles);
+console.log(articles.length);
+
+let seenLinks = [];
+
+articles.forEach(async (article) => {
+  if (seenLinks.includes(article.link)) {
+    await deleteArticle(article._id);
+  } else {
+    seenLinks.push(article.link);
+  }
+});
+
 /*
 1. Нужно создать метод, который будет удалять статью (мы создаём функцию в файле db.js).  
 2. Нужно написать алгоритм, который будет находить "дубли".  
